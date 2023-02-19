ddaf29b8b2fd21b5b2df3f7be2109a5efa8cd569
lu-castro <luciacastro.23.5@gmail.com>
2023-02-19 01:46:09 -0300

    agregando dirección de mail al formulario

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 document.getElementById("presentacionId").onclick = function (){
     document.getElementById("presentacionId").textContent = "Otras de mis habilidades son: Lenguajes Javascript, PHP, NodeJS, HTML5, CSS3 - Bases de Datos: SQL y MySQL - Idiomas español e inglés (B2) - Copywriting - Time management - Ilustración 2D - Sketch design tool - Adobe Photoshop - Microsoft Word - Excel ";
 };
+
+//MODIFICANDO LA IMAGEN DE LA PRESENTACION
